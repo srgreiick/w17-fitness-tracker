02ca093e52b632b90349c7c4a8982986fbc5bc85
sgreilick <samgreilick@gmail.com>
2020-09-24 19:00:52 -0600

    model fixed, now objects are being built and stroed properly, app deployed to heroku

--- models/exercise.js
+++ models/exercise.js
@@ -3,42 +3,56 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-    type: {
-        type: String,
-        //required: true,
-        trim: true
+    day:{
+        type: Date,
+        default: new Date
     },
-    name: {
-        type: String,
-        //required: true,
-        trim: true
-    },
-    distance: {
-        type: Number,
-        //required: true,
-        trim: true
-    },
-    weight: {
-        type: Number,
-        //required: true,
-        trim: true
-    },
-    sets: {
-        type: Number,
-        //required: true,
-        trim: true
-    },
-    reps: {
-        type: Number,
-        //required: true,
-        trim: true
-    },
-    duration: {
-        type: Number,
-        //required: true,
-        trim: true
-    }
+    exercises:[ {
+
+        type: {
+            type: String,
+            //required: true,
+            trim: true
+        },
+        name: {
+            type: String,
+            //required: true,
+            trim: true
+        },
+        distance: {
+            type: Number,
+            //required: true,
+            trim: true
+        },
+        weight: {
+            type: Number,
+            //required: true,
+            trim: true
+        },
+        sets: {
+            type: Number,
+            //required: true,
+            trim: true
+        },
+        reps: {
+            type: Number,
+            //required: true,
+            trim: true
+        },
+        duration: {
+            type: Number,
+            //required: true,
+            trim: true
+        }
+    }]
 });
+
+//Taken from session 18, thank you
+ExerciseSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+})
 
 const workoutExercisePlan = mongoose.model("workoutPlan", ExerciseSchema);
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,31 +3,35 @@ const db = require("../models");
 
 
 module.exports = function (app) {
-    app.get("/api/workouts",function (req, res) {
-        
-    })
+  app.get("/api/workouts", function (req, res) {
+    db.workoutPlan.find({})
+      .then(data => { res.json(data)
+        console.log(data); })
+      
+      // .catch(err => {
+      //   res.json(err);
+      // });
+  })
 
 
-    app.post("/api/workouts",function (req, res) {
-        db.workoutPlan.create({})
-        .then(data => {
-            res.json(data);
-          })
-          .catch(err => {
-            res.json(err);
-          });
-    })
+  app.post("/api/workouts", function (req, res) {
+    db.workoutPlan.create({})
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  })
 
 
-    app.put("/api/workouts/:id",function ({body,params}, res) {
-        
-        db.workoutPlan.findOneAndUpdate({_id:params.id }, { $push: {exercises: body } }, { new: true })
-        .then(data => {
-          res.json(data);
-        })
-        .catch(err => {
-          res.json(err);
-        });
-    })
-
+  app.put("/api/workouts/:id", function ({ body, params }, res) {
+    db.workoutPlan.findOneAndUpdate({ _id: params.id }, { $push: { exercises: body } }, { new: true })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  })
 }
